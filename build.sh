504361c08a3e2ad95d174bc25c15457d57d7837f
Carlos Santos <santos@optimal-systems.de>
2023-02-01 14:09:33 +0100

    bump version to 1.2.4

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 #
 mvn clean package 
-VERSION=$(mvn help:evaluate  -Dexpression=project.version -q  -DforceStdout 2>/dev/null)
+#VERSION=$(mvn help:evaluate  -Dexpression=${project.version} -q  -DforceStdout 2>/dev/null)
+#VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" "pom.xml")
+eval $(printf 'VERSION=${project.version}\n' | mvn  help:evaluate 2>/dev/null | grep 'VERSION')
 docker build --build-arg VERSION=${VERSION} -t machamba/java-demo-app:${VERSION} .
